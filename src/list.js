ad96d80d2116554a583855f24217f108be7fdb1a
richtepa <paul@richter.dev>
2022-09-10 17:09:35 +0200

    removed comments and empty lines

--- src/list.js
+++ src/list.js
@@ -14,17 +14,8 @@ window.onload = function(){
 
 
 async function showAllCitis(){
-    //let list = await citiList.getList();
     let pages = await helper.loadData();
-
     ui = new UI(document.getElementById("list"), pages, save);
-    
-    /*
-    for(let page of pages){
-        citiUI.addPageToUI(page);
-    }
-    citiUI.eventListeners();
-    */
 }
 
 async function save(data){
